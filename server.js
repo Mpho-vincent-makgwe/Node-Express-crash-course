dbcf8c36fa3e73f50d2cddadce244a0676d46265
Mpho-vincent-makgwe <mphomakgwe4@gmail.com>
2024-02-26 16:50:31 +0200

    Learning MiddleWare

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use((req,res,next) =>{
+    console.log("<h1>HELLLOOO</h1>");
+    next();
+})
 app.get('/', (req, res)=>{
-    res.send('Getting root')
-});
-
-app.get('/profile', (req, res)=>{
-    res.send('Getting profile')
-});
-
-app.post('/profile', (req, res)=>{
     const user = {
         name: "Siphosenkosi Vincent",
         surname: "Senkosi",
